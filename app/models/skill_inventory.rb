ad1bc747576f2cc6a09a32f322d1e7d44f7aa6e8
Tyler Graham <Tyler@Tylers-MacBook-Pro.local>
2015-05-19 22:42:10 -0600

    Initial CRUD completed

--- app/models/skill_inventory.rb
+++ app/models/skill_inventory.rb
@@ -1,5 +1,49 @@
+require 'yaml/store'
+require_relative 'skill'
+
 class SkillInventory
   def self.database
-    @database ||= YAML::Store.new("db/skill_inventory")
+    @database ||= YAML::Store.new("db/skill_storage")
+  end
+
+  def self.create(skill)
+    database.transaction do
+      database['skills'] ||= []
+      database['total'] ||= 0
+      database['total'] += 1
+      database['skills'] << { "id" => database['total'], "title" => skill[:title], "description" => skill[:description] }
+    end
+  end
+
+  def self.raw_skills
+    database.transaction do
+      database['skills'] || []
+    end
+  end
+
+  def self.all
+    raw_skills.map { |data| Skill.new(data) }
+  end
+
+  def self.raw_skill(id)
+    raw_skills.find { |skill| skill["id"] == id }
+  end
+
+  def self.find(id)
+    Skill.new(raw_skill(id))
+  end
+
+  def self.update(id, skill)
+    database.transaction do
+      target = database['skills'].find { |data| data["id"] == id }
+      target["title"] = skill[:title]
+      target["description"] = skill[:description]
+    end
+  end
+
+  def self.delete(id)
+    database.transaction do
+      database['skills'].delete_if { |skill| skill["id"] == id }
+    end
   end
 end
